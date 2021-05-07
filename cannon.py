d942062f086a2917fb61fc24ca61294a0076555e
luisferro2 <a01378248@itesm.mx>
2021-05-07 14:06:42 -0500

    Revisar indentación correcta siguiendo la convención de estilo PEP-8.

--- cannon.py
+++ cannon.py
@@ -1,5 +1,8 @@
 from random import randrange
-from turtle import *
+from turtle import clear, goto, dot,       \
+                   update, ontimer, setup, \
+                   hideturtle, up, tracer, \
+                   onscreenclick, done
 from freegames import vector
 
 ball = vector(-200, -200)
@@ -11,8 +14,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 5#(x + 200) / 25# 200/25
-        speed.y = (y + 200) / 5 #(y + 200) / 25
+        speed.x = (x + 200) / 7#(x + 200) / 25# 200/25
+        speed.y = (y + 200) / 7 #(y + 200) / 25
 
 def inside(xy):
     "Return True if xy within screen."
@@ -55,7 +58,7 @@ def move():
 
     draw()
 
-    #Reposition balls when they reach the edge of the screen 
+    #Reposition balls when they reach the edge of the screen
     for target in targets:
         if not inside(target):
             target.x = randrange(-150, 150)
